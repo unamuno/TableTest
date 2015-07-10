e444659f4ba615354510329fb61dfd155187bfb7
Merlin <merlin@abindenofen.de>
2015-07-10 21:15:20 +0200

    Ich versuche das jTable Ellement zu verstehen.

---
+++ TableTest/src/tabletest/TableTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabletest;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Merlin
+ */
+public class TableTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AddTable add = new AddTable();
+        GuiTable tab = new GuiTable();
+        tab.setVisible(true);
+        DefaultTableModel model = (DefaultTableModel) tab.tbArtikelTabelle.getModel();
+        model.addRow(add.textFieldInputs);
+    }
+    
+}
